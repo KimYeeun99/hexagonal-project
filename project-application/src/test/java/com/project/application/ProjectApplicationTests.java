73d5822f3f0ba582f81db9a4ee1906542e17a7c4
KimYeeun99 <kyz0608@naver.com>
2025-03-20 23:33:23 +0900

    init: 테스트 환경 구축 진행중

--- project-application/src/test/java/com/project/application/ProjectApplicationTests.java
+++ project-application/src/test/java/com/project/application/ProjectApplicationTests.java
@@ -5,9 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
--- project-input-http/src/test/java/com/project/http/ProjectInputHttpApplicationTests.java
+++ project-input-http/src/test/java/com/project/http/ProjectInputHttpApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjectInputHttpApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
